14639a2bb1a2d6ff2e36093b777182973b12869f
Cacerespedro94 <cacerespedro94@gmail.com>
2021-04-30 00:17:54 -0300

    First commit

---
+++ Backend/src/routes/product.routes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const Product = require('../models/product');
+
+router.get('/', async (req, res) =>{
+    const products = await Product.find();
+    res.json(products);
+});
+
+router.get('/:id', async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    res.json(product);
+})
+
+router.post('/', async (req, res) => {
+    const { name, description, price, stock, imageUrl } = req.body;
+    const product = new Product({name, description, price, stock, imageUrl});
+    await product.save() ;
+    res.json({status: 'Product saved'});
+});
+
+router.put('/:id', async (req, res) => {
+    const { title, description } = req.body;
+    const newProduct = {title, description};
+    await Product.findByIdAndUpdate(req.params.id, newProduct);
+    res.json({status: 'Product updated'});
+})
+
+router.delete('/:id', async (req, res) => {
+    await  Product.findByIdAndRemove(req.params.id);
+    res.json({status: 'Product delete'});
+})
+module.exports = router;
