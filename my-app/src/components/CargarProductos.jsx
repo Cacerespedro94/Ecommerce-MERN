582b0a39b04871a9ea3e7c40d16371861c7245f3
Cacerespedro94 <cacerespedro94@gmail.com>
2021-05-01 23:38:35 -0300

    Add a form with validations

--- my-app/src/components/CargarProductos.jsx
+++ my-app/src/components/CargarProductos.jsx
@@ -1,35 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
+import  { useForm } from 'react-hook-form'
+
+
 
 const CargarProductos = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [Productos, setProductos] = useState([]);
+   
+   
+    const onSubmit = (data,e) => {
+    
+        console.log(data)
+        
+    e.target.reset();
+}
   return (
  <div className="container mt-5">
      <h1 className="text-center">Ingreso de stock</h1>
-     <form>
+     <form onSubmit={handleSubmit(onSubmit)}>
          
          <div className="row mt-4">
              <div className="col-6">
-                <input name="title" className="form-control" type="text" placeholder="Nombre del producto"/>
+                <input name="name" className="form-control" type="text" placeholder="Nombre del producto"
+            
+                {
+                    ...register("name",{ required: true} )
+                }
+                />
+                {errors.name && "Ingresar el nombre"}
              </div>
              <div className="col-6">
-                <input name="title" className="form-control" type="text" placeholder="Descripcion"/>
+                <input name="description" className="form-control" type="text" placeholder="Descripcion"
+                {
+                    ...register("description",{ required: true, message: 'Campo obligatorio' } )
+                }
+                />
+                {errors.name && "Complete la descripción"}
              </div>
          </div>
          <div className="row mt-4">
              <div className="col-6">
-                <input name="title" className="form-control" type="text" placeholder="Cantidad"/>
+                <input name="stock" className="form-control" type="text" placeholder="Cantidad"
+                {
+                    ...register("stock",{ required: true, message: 'Campo obligatorio' } )
+                }
+                />
+                {errors.name && "Complete el stock"}
              </div>
              <div className="col-6">
-                <input name="title" className="form-control" type="text" placeholder="Precio"/>
+                <input type="number" name="price" className="form-control" type="text" placeholder="Precio"
+                    {
+                        ...register("price",{ required: true, min: 0 , max: 10, message: 'Campo obligatorio' } )
+                    }
+                />
+                {errors.name && "Complete el precio"}
+                
              </div>
          </div>
-         <div className="row mt-4">
+         <div className="row mt-4"> 
              <div className="col-6">
-                <input name="title" className="form-control" type="text" placeholder="Categoria"/>
+                <input name="category" className="form-control" type="text" placeholder="Categoria"
+                {
+                    ...register("category",{ required: true, message: 'Campo obligatorio' } )
+                }
+                />
+                {errors.name && "Complete la categoria"}
              </div>
              <div className="col-6">
-               <input name="title" className="form-control" type="text" placeholder="Imágen"/>
+               <input name="imageUrl" className="form-control" type="text" placeholder="Imágen"
+                {
+                    ...register("imageUrl",{ required: true, message: 'Campo obligatorio' } )
+                }
+               />
+               {errors.name && "Complete la imagen."}
              </div>
          </div>
+         <button type='submit' width="100%" className='btn btn-primary mt-3 '>Aceptar</button>
      </form>
 
  </div>
